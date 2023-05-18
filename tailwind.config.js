df95fa6dcbd40c6fd1ec45880c4d1e6ec3542237
kamaluiuxd <kamaluiuxd@gmail.com>
2023-05-18 15:47:42 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./public/**/*.{html,js}"],
+	// purge: ["./public/**/*.{html,js}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
